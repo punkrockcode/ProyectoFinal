1af5996c8f8768077fea3d1d87afbfef8dbbfd09
punkrockcode <schianea@outlook.com>
2023-02-28 23:14:26 -0300

    Se agrega index, css y javascript

---
+++ texto.js
@@ -0,0 +1,18 @@
+function myFunction1() {
+    document.getElementById("datos").textContent = 
+    "Datos Personales: " + " Nombre: Travis Lucas"
+    + " Email: travis.lucas@ejemplo.com" +
+    " Fecha de nacimiento: 13/7/1977" + 
+    " Nacionalidad: Argentino" + 
+    " Direccion: 9945 Homestead Rd" + 
+    " Telefono: (727) 573-6902"
+    
+     }
+
+function myFunction2() {
+        document.getElementById("datos").textContent = "Mas de 2 años de experiencia trabajando como freelancer. 1 año trabajando para empresa full time";
+        
+         }     
+function myFunction3() {
+            document.getElementById("datos").textContent = "Proactivo, responsable, manejo de clientes, armado de presupuestos.";
+             }         
